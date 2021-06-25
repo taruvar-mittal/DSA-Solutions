6c0287149941f805304ccc0ff7b97d66d576148d
taruvar-mittal <mittaltaruvar@gmail.com>
2021-06-25 11:51:16 +0530

    25 June

---
+++ Hashmap/maximum frequency stack - good ques.cpp
@@ -0,0 +1,61 @@
+/*
+Leetcode 895. Maximum Frequency Stack
+ques:-
+Design a stack-like data structure to push elements to the stack and pop the most frequent element from the stack.
+
+Implement the FreqStack class:
+
+FreqStack() constructs an empty frequency stack.
+void push(int val) pushes an integer val onto the top of the stack.
+int pop() removes and returns the most frequent element in the stack.
+If there is a tie for the most frequent element, the element closest to the stack's top is removed and returned.
+ 
+
+Example 1:
+
+Input
+["FreqStack", "push", "push", "push", "push", "push", "push", "pop", "pop", "pop", "pop"]
+[[], [5], [7], [5], [7], [4], [5], [], [], [], []]
+Output
+[null, null, null, null, null, null, null, 5, 7, 5, 4]
+
+Explanation
+FreqStack freqStack = new FreqStack();
+freqStack.push(5); // The stack is [5]
+freqStack.push(7); // The stack is [5,7]
+freqStack.push(5); // The stack is [5,7,5]
+freqStack.push(7); // The stack is [5,7,5,7]
+freqStack.push(4); // The stack is [5,7,5,7,4]
+freqStack.push(5); // The stack is [5,7,5,7,4,5]
+freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,5,7,4].
+freqStack.pop();   // return 7, as 5 and 7 is the most frequent, but 7 is closest to the top. The stack becomes [5,7,5,4].
+freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,4].
+freqStack.pop();   // return 4, as 4, 5 and 7 is the most frequent, but 4 is closest to the top. The stack becomes [5,7].
+*/
+
+class FreqStack
+{
+public:
+    unordered_map<int, int> freq;
+    priority_queue<vector<int>> pq;
+    int size = 0;
+
+    // pq pops the element with max freq, and if freq is same it pops the element with greater index i.e. the element that was pushed later into the stack.
+    FreqStack()
+    {
+    }
+
+    void push(int val)
+    {
+        freq[val]++;
+        pq.push({freq[val], size++, val});
+    }
+
+    int pop()
+    {
+        vector<int> curr = pq.top();
+        pq.pop();
+        freq[curr[2]]--;
+        return curr[2];
+    }
+};
